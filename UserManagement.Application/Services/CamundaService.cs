3510e4431494ac03a68c31946af1596aa58b22d4
Sujata Telang <Sujata.Telang@primeroedge.com>
2025-03-06 11:05:32 +0530

    add startprocess method to service and controller add asset entity

--- UserManagement.Application/Services/CamundaService.cs
+++ UserManagement.Application/Services/CamundaService.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
 using System.Text;
-using System.Threading.Tasks;
+using UserManagement.Domain.Entities;
 
 namespace UserManagement.Application.Services
 {
-    internal class CamundaService
+    public class CamundaService
     {
+        private readonly HttpClient _httpClient;
+        public CamundaService(HttpClient httpClient) {
+            _httpClient = httpClient;
+        }
+
+        public async Task StartProcess(string processKey, Asset asset)
+        {
+            var requestBody = new
+            {
+                variables = new
+                {
+                    assetId = new { value = asset.AssetId, type = "integer" },
+                    assetName = new { value = asset.AssetName, type = "String" },
+                    assetAddress = new { value = asset.AssetAddress, type = "String" }
+                }
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"http://localhost:8080/engine-rest/process-definition/key/{processKey}/start", content);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Process Started: {result}");
+        }  
     }
 }
